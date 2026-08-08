718e971d8ac831e986cafe338b46c537352ac5a2
yueyi <email@weiwenhuaming.com>
2026-08-08 19:54:16 +0800

    feat:add health function

--- internal/application/health_svc.go
+++ internal/application/health_svc.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	"gateway/internal/client/rpc"
-	"gateway/internal/client/rpc/core-rpc/healthpb"
 	"gateway/internal/model/dto"
 )
 
@@ -16,9 +15,5 @@ func (s *HealthService) Ping(ctx context.Context) (*dto.PingResponse, error) {
 	ctx, cancel := context.WithTimeout(ctx, 5e9)
 	defer cancel()
 
-	resp, err := s.rpc.GetHealthClient().Ping(ctx, &healthpb.PingRequest{})
-	if err != nil {
-		return nil, err
-	}
-	return &dto.PingResponse{Message: resp.GetMessage()}, nil
+	return &dto.PingResponse{Message: "success"}, nil
 }
--- internal/facade/controller/health_ctrl.go
+++ internal/facade/controller/health_ctrl.go
@@ -3,7 +3,6 @@ package controller
 import (
 	"gateway/internal/application"
 	"gateway/internal/model/reponse"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -18,7 +17,6 @@ func NewHealthController(svc *application.HealthService) *HealthController {
 func (h *HealthController) Ping(c *gin.Context) {
 	resp, err := h.svc.Ping(c.Request.Context())
 	if err != nil {
-		log.Printf("health ping: %v", err)
 		reponse.Fail(c, http.StatusBadGateway, "core-server unavailable")
 		return
 	}
